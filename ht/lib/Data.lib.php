24bb27a81c4df2561b713115ffcfac9853f08da1
changbof <aboooo@139.com>
2016-07-20 18:45:53 +0800

    modify for Data.lib.php

--- ht/lib/Data.lib.php
+++ ht/lib/Data.lib.php
@@ -15,6 +15,7 @@ class Data
      */
 
 
+
     /** K3和值
      * @param $bets
      * @param $kj
@@ -28,30 +29,22 @@ class Data
         return count($r);
     }
 
-    // K3三同号单选 todo
+    // K3三同号单选
     public function k33dx($betData, $kjData){
-        $betData = str_replace('*','',$betData);
-        $betData = explode(',',$betData);
-        $kjData = explode(',',$kjData);
-
-        $kj1 = intval($kjData[0]) + intval($kjData[1]);
-        $kj2 = intval($kjData[2]);
-        $kj = $kj1.','.$kj2;
-        return strpos($betData, $kjData)!=false ? 1 : 0;
-
+        $kjData = str_replace(',','',$kjData);
+        return !stripos($betData, $kjData) ? 1 : 0;
 
     }
 
     // K3三同号通选
     public function k33ttx($betData, $kjData){
-        $kjData = str_replace(',','',$kjData);
-        return strpos($betData, $kjData)!=false ? 1 : 0;
+        return self::k33dx($betData, $kjData);
     }
 
     // K3三连号通选
     public function k33ltx($betData, $kjData){
 
-        return self::k33ttx($betData, $kjData);
+        return self::k33dx($betData, $kjData);
     }
 
     // K3三不同号
@@ -109,6 +102,17 @@ class Data
 
     // K3二同号复选
     public function k32tfx($bet){
+
+        $betData = str_replace('*','',$betData);
+        $betData = explode(',',$betData);
+        $kjData = explode(',',$kjData);
+
+        $kj1 = intval($kjData[0]) + intval($kjData[1]);
+        $kj2 = intval($kjData[2]);
+        $kj = $kj1.','.$kj2;
+        return strpos($betData, $kjData)!=false ? 1 : 0;
+
+
         $check=array('11*','22*','33*','44*','55*','66*');
         $bet1=explode(' ', $bet);$a=array_unique($bet1);
         if(count($bet1)!=count($a) || count($bet1)>6 || count($bet1)<1) return 0;
@@ -131,5 +135,102 @@ class Data
             if(!in_array($y,$check2)) return 0;
         }
         return count($bet[0])*count($bet[1]);
+    }
+
+    /**
+     * 常用算法
+     */
+
+
+
+    /** 组合
+     * @param $array            备选数组
+     * @param $num              选取的个数
+     * @return array            组合
+     */
+    public function combine($array, $num){
+        $c = count($ar);
+        if ($n>$c) return false; // parameter wrong
+        if ($c>50) return false; // too big array :)
+
+        $r = array();
+
+        $code = "";
+        $list = array();
+        for($i=1;$i<=$n;$i++){
+            $list[] = '$v'.$i;
+            $code .= 'foreach($ar as $k'.$i.'=>$v'.$i.'){';
+            if($i!=$n) $code .= 'unset($ar[$k'.$i.']);';
+        }
+        $code .= '$t = array('.join(',',$list).');';
+        $code .= 'sort($t);';
+        $code .= '$r[] = join(",",$t);';
+        for($i=$n-1;$i>0;$i--){
+            $code .= '}$ar[$k'.$i.']=$v'.$i.';';
+        }
+        $code .= '}';
+        eval($code);
+        return array_values(array_unique($r));
+    }
+
+    /**
+     * 组合算法
+     * @param array $elements    备选数组
+     * @param $chosen            选取的个数
+     * @return array             组合
+     */
+    function array_combination(array $elements, $chosen)
+    {
+        $result = array();
+
+        for ($i = 0; $i < $chosen;   $i++) { $vecm[$i] = $i; }
+        for ($i = 0; $i < $chosen-1; $i++) { $vecb[$i] = $i; }
+        $vecb[$chosen - 1] = count($elements) - 1;
+        $result[] = $vecm;
+
+        $mark = $chosen - 1;
+        while (true) {
+            if ($mark == 0) {
+                $vecm[0]++;
+                $result[] = $vecm;
+                if ($vecm[0] == $vecb[0]) {
+                    for ($i = 1; $i < $chosen; $i++) {
+                        if ($vecm[$i] < $vecb[$i]) {
+                            $mark = $i;
+                            break;
+                        }
+                    }
+                    if (($i == $chosen) && ($vecm[$chosen - 1] == $vecb[$chosen - 1])) { break; }
+                }
+            } else {
+                $vecm[$mark]++;
+                $mark--;
+                for ($i = 0; $i <= $mark; $i++) {
+                    $vecb[$i] = $vecm[$i] = $i;
+                }
+                $vecb[$mark] = $vecm[$mark + 1] - 1;
+                $result[] = $vecm;
+            }
+        }
+
+        return $result;
+    }
+    /**
+     * 常用算法：zx
+     *
+     * @params bet		投注列表：1 2 3 4 5 6
+     * @params data		开奖所需的号码：4,5,2
+     *
+     * @return 			返回中奖注数
+     */
+    public function zx($bet,$data){
+        $data = explode(',',$data);
+        $bet = explode(' ',$bet);
+        $rst = combine($bet,count($data));
+        foreach($rst as $r) {
+            array_filter($r, funcion($var){
+                return !in_array($data,$r);
+            });
+        }
     }
 }
